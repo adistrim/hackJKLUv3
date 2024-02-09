c8ccbfe4d1ac2805f4a4434e20a02d120a773325
Aditya Raj <adistrim.dev@gmail.com>
2024-02-10 01:53:27 +0530

    added code of conduct and updated the registerbtn

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,68 +1,119 @@
 "use client";
-import React from 'react';
-import NextLink from 'next/link';
-import RegisterBtn from './registerbtn';
+import React from "react";
+import NextLink from "next/link";
+import RegisterBtn from "./registerbtn";
+import { Share_Tech_Mono } from "next/font/google";
+
+const tech_mono = Share_Tech_Mono({ subsets: ["latin"], weight: ["400"] });
 
 const Footer: React.FC = () => {
+  return (
+    <footer className={`bg-[#121212] mt-[5rem] text-white p-5 md:p-8`}>
+      <div className="md:mx-[3rem]">
+        <div className="flex flex-col md:flex-row justify-between items-start">
+          {/* Left Side */}
+          <div className="w-full md:w-50% lg:40% flex flex-col">
+            <RegisterBtn />
+            <NextLink href="https://devfolio.co/code-of-conduct" passHref target="_blank">
+              <p className={`text-[0.9rem] mt-[2rem] md:text-[1.2rem] font-thin text-gray-400 hover:text-white hover:underline ${tech_mono.className}`}>
+                Code of conduct
+              </p>
+            </NextLink>
 
-    return (
-        <footer className="bg-[#121212] mt-[5rem] text-white p-5 md:p-8">
-            <div className='md:mx-[3rem]'>
-                <div className="flex flex-col md:flex-row justify-between items-start">
-                    {/* Left Side */}
-                    <div className="w-full md:w-50% lg:40% flex flex-col">
-                        
-                        <RegisterBtn />
-
-                        <div className="mt-[2rem] md:mr-[7rem] mb-[2rem]">
-                            <h4 className="text-[1rem] md:text-[1.5rem] text-[#E16931] mb-[1.5rem]">ADDRESS</h4>
-                            <p className='text-[0.7rem] md:text-[1.1rem] font-thin'>JK LAKSHMIPAT UNIVERSITY, P.O. 302 026, MAHAPURA RD, NEAR MAHINDRA SEZ, MAHAPURA, RAJASTHAN 302026</p>
-                        </div>
-
-                        <div className="my-4">
-                            <h4 className="text-[1rem] md:text-[1.5rem] text-[#E16931] font-base mb-[1.5rem]">CONTACT INFO</h4>
-                            <a className='md:mb-[0.5rem] text-[0.7rem] md:text-[1.2rem] font-thin' href='mailto:counciloftechnicalaffairs@jklu.edu.in'>Email: counciloftechnicalaffairs@jklu.edu.in</a>
-                            <p className='md:mb-[1rem] text-[0.7rem] md:text-[1.2rem] font-thin'>Phone:<br /> Sponsorship: +91 98285 35479<br />Registrations: +91 86199 60229</p>
-                            <a href="https://www.instagram.com/smart_tech_club/" className="text-white font-thin  text-[0.7rem] md:text-[1.2rem]">Follow us:
-                                <img src="instagram-logo.png" alt="Instagram" className="h-6 md:h-8 inline-block" />
-                            </a>
-                        </div>
-                    </div>
-
-                    {/* Right Side (Google Maps) */}
-                    <div className="w-full md:w-50% lg:w-60%">
-                        <div>
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.117070008914!2d75.64722912457951!3d26.836228513374916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396c4af4fe68f403%3A0x3bf05f95df22b8c4!2sJK%20Lakshmipat%20University!5e0!3m2!1sen!2sin!4v1695563431231!5m2!1sen!2sin"
-                                width="100%"
-                                height="100%"
-                                className='relative w-full h-[15rem] md:h-[15rem] md:w-[25rem] lg:h-[25rem] lg:w-[45rem] rounded-md'
-                            />
-                        </div>
-                    </div>
-                </div>
-
-                {/* Bottom Section */}
-                <div className="flex flex-col md:flex-row justify-between items-center mt-8">
-                    <div className='flex flex-row items-center'>
-                        <img src="hackjklu-logo.png" alt="Logo" className='h-8 md:h-16' />
-                        <img src="hackjklu-text.png" alt="HackJKLU" className='h-6 md:h-12 ml-2 mt-2' />
-                    </div>
-                    <p className='my-[1rem] text-[0.7rem] md:text-base mx-auto'>&copy;2024 HackJKLU. All rights reserved.</p>
-                    <div>
-                        <span className="text-gray-400 text-[0.7rem] md:text-sm">Made with ❤️ by </span>
-                        <NextLink href="https://github.com/adistrim/" target='_blank' passHref>
-                            <span className="text-gray-400 text-[0.7rem] md:text-sm">@adistrim</span>
-                        </NextLink>
-                        <span className="text-gray-400 text-[0.7rem] md:text-sm"> | </span>
-                        <NextLink href="https://github.com/adistrim/hackJKLUv3/" target='_blank' passHref>
-                            <span className="text-gray-400 text-[0.7rem] md:text-sm">source code</span>
-                        </NextLink>
-                    </div>
-                </div>
+            <div className="mt-[2rem] md:mr-[7rem] mb-[2rem]">
+              <h4 className="text-[1rem] md:text-[1.5rem] text-[#E16931] mb-[1.5rem]">
+                ADDRESS
+              </h4>
+              <p className="text-[0.7rem] md:text-[1.1rem] font-thin">
+                JK LAKSHMIPAT UNIVERSITY, P.O. 302 026, MAHAPURA RD, NEAR
+                MAHINDRA SEZ, MAHAPURA, RAJASTHAN 302026
+              </p>
             </div>
-        </footer>
-    );
+
+            <div className="my-4">
+              <h4 className="text-[1rem] md:text-[1.5rem] text-[#E16931] font-base mb-[1.5rem]">
+                CONTACT INFO
+              </h4>
+              <a
+                className="md:mb-[0.5rem] text-[0.7rem] md:text-[1.2rem] font-thin"
+                href="mailto:counciloftechnicalaffairs@jklu.edu.in"
+              >
+                Email: counciloftechnicalaffairs@jklu.edu.in
+              </a>
+              <p className="md:mb-[1rem] text-[0.7rem] md:text-[1.2rem] font-thin">
+                Phone:
+                <br /> Sponsorship: +91 98285 35479
+                <br />
+                Registrations: +91 86199 60229
+              </p>
+              <a
+                href="https://www.instagram.com/smart_tech_club/"
+                className="text-white font-thin  text-[0.7rem] md:text-[1.2rem]"
+              >
+                Follow us:
+                <img
+                  src="instagram-logo.png"
+                  alt="Instagram"
+                  className="h-6 md:h-8 inline-block"
+                />
+              </a>
+            </div>
+          </div>
+
+          {/* Right Side (Google Maps) */}
+          <div className="w-full md:w-50% lg:w-60%">
+            <div>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.117070008914!2d75.64722912457951!3d26.836228513374916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396c4af4fe68f403%3A0x3bf05f95df22b8c4!2sJK%20Lakshmipat%20University!5e0!3m2!1sen!2sin!4v1695563431231!5m2!1sen!2sin"
+                width="100%"
+                height="100%"
+                className="relative w-full h-[15rem] md:h-[15rem] md:w-[25rem] lg:h-[25rem] lg:w-[45rem] rounded-md"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Bottom Section */}
+        <div className="flex flex-col md:flex-row justify-between items-center mt-8">
+          <div className="flex flex-row items-center">
+            <img src="hackjklu-logo.png" alt="Logo" className="h-8 md:h-16" />
+            <img
+              src="hackjklu-text.png"
+              alt="HackJKLU"
+              className="h-6 md:h-12 ml-2 mt-2"
+            />
+          </div>
+          <p className="my-[1rem] text-[0.7rem] md:text-base mx-auto">
+            &copy;2024 HackJKLU. All rights reserved.
+          </p>
+          <div>
+            <span className="text-gray-400 text-[0.7rem] md:text-sm">
+              Made with ❤️ by{" "}
+            </span>
+            <NextLink
+              href="https://github.com/adistrim/"
+              target="_blank"
+              passHref
+            >
+              <span className="text-gray-400 text-[0.7rem] md:text-sm">
+                @adistrim
+              </span>
+            </NextLink>
+            <span className="text-gray-400 text-[0.7rem] md:text-sm"> | </span>
+            <NextLink
+              href="https://github.com/adistrim/hackJKLUv3/"
+              target="_blank"
+              passHref
+            >
+              <span className="text-gray-400 text-[0.7rem] md:text-sm">
+                source code
+              </span>
+            </NextLink>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/app/components/registerbtn.tsx
+++ src/app/components/registerbtn.tsx
@@ -6,7 +6,7 @@ const tech_mono = Share_Tech_Mono({ subsets: ["latin"], weight: ["400"] });
 
 const RegisterBtn: React.FC = () => {
     return (
-        <NextLink href="https://hack-jklu.devfolio.co/" passHref>
+        <NextLink href="https://hack-jklu.devfolio.co/" passHref target="_blank">
             <p className={`inline-flex px-8 py-3 text-black text-[1.1rem] md:text-[1.3rem] justify-center items-center gap-4 rounded-md bg-gradient-to-r from-red-600 to-yellow-500 ${tech_mono.className}`}>
                 Register Now
             </p>
