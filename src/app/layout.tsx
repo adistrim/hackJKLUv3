d70cbef510e7d4e45dd8805f213842b1f0430b5c
Aditya Raj <adistrim.dev@gmail.com>
2024-02-14 16:12:40 +0530

    updated the metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "HackJKLU",
   description:
-    "Come be a part of HackJKLU 3.0, the hackathon organized by the Smart Tech Club of JK Lakshmipat University. Join us from 15th to 17th March 2024 for a thrilling experience in web2, web3, IoT, game dev, AR/VR, cybersecurity, AI/ML, and open innovation. Register now!",
+    "Come be a part of HackJKLU v3.0, the hackathon organized by the Council of Technical Affairs of JK Lakshmipat University, Jaipur. Join us from 15th to 17th March 2024 for a thrilling hackathon in Web2, Web3, IoT, Game dev/ AR/ VR, Cybersecurity and AI/ML. Register now!",
   keywords:
     "HackJKLU, HackJKLU 3.0, HackJKLU v3.0, hackjklu, JKLU hackathon, web development hackathon, AI/ML hackathon, cybersecurity challenge, game development hackathon, AR/VR hackathon, IoT hackathon, open innovation, JK Lakshmipat University, Jaipur, Rajasthan, India, hackathon, Jaipur hackathon, hackathon in march, hackathon in 2024, hackathon in India, hackathon in Rajasthan, hackathon in Jaipur, hackathon in JKLU, hackathon in JK Lakshmipat University, hackathon in JKLU Jaipur, hackathon in JKLU Rajasthan, hackathon in JKLU India, hackathon in JKLU Jaipur Rajasthan India, hackathon in JKLU Jaipur Rajasthan, hackathon in JKLU Jaipur India, hackathon in JKLU",
 };
